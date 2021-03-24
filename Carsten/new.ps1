35a592b3113c9a244b8064ea58556d5ffea22372
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-03-24 14:07:03 -0700

    Update new.ps1

--- Carsten/new.ps1
+++ Carsten/new.ps1
@@ -1,5 +1,8 @@
 netsh interface ip set address "Ethernet" static 192.168.1.10 255.255.255.0 192.168.1.1
 netsh interface ip set dns "Ethernet" static 127.0.0.1
 
-$input = Read-Host -Prompt 'Please type in the new name for this computer: '
+$input = Read-Host -Prompt 'Please type in the new name for this computer'
 Rename-Computer -NewName "$input"
+
+Add-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName corp.globexpower.com -InstallDNS
