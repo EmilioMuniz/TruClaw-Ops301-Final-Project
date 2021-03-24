60d6d8047fb992aca1f60a2d560e0be6963b52d1
Carsten Rossen <69820600+carsten-rossen@users.noreply.github.com>
2021-03-24 11:59:11 -0700

    Create new.ps1
    new commits

---
+++ Carsten/new.ps1
@@ -0,0 +1,5 @@
+netsh interface ip set address "Ethernet" static 192.168.1.10 255.255.255.0 192.168.1.1
+netsh interface ip set dns "Ethernet" static 127.0.0.1
+
+$input = Read-Host -Prompt 'Please type in the new name for this computer: '
+Rename-Computer -NewName "$input"
