e26b0475940dccc584f5e770ee8cf403cc7255d8
Emilio Muniz <emiliomunizphoto@gmail.com>
2021-03-25 11:38:08 -0700

    Update UsersScript.ps1

--- Emilio/UsersScript.ps1
+++ Emilio/UsersScript.ps1
@@ -1,3 +1,13 @@
+# Script:                   UsersScript.ps1                       
+# Author:                   Emilio Muniz                       
+# Date of latest revision:  3/25/2021    
+# Purpose:                  Use New-ADUser to create new user with attributes.
+
+# Declaration of variables
+
+# Declaration of functions
+
+# Main
 Import-Module ActiveDirectory
 $file = "C:\Users\Administrator\Desktop\UsersNames.csv"
 $importedUsers = Import-csv $file
@@ -15,3 +25,5 @@ foreach ($user in $importedUsers)
  $title = $user.title
  New-ADUser -Name "$givenName" -Surname "$sn" -SamAccountName "$sAMAccontName" -Title "$title"
 }
+# End
+# References: https://activedirectorypro.com/create-bulk-users-active-directory/ https://www.manageengine.com/products/ad-manager/help/csv-import-management/active-directory-ldap-attributes.html
